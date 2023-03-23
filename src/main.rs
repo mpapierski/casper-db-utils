ee73011bc500b248efae31a5ddaf0c989f6a5154
Michał Papierski <michal@papierski.net>
2023-03-23 19:32:55 +0100

    Purge WIP

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,7 @@ use std::{fs::OpenOptions, process};
 use clap::{crate_description, crate_version, Arg, Command};
 use log::error;
 
-use subcommands::{archive, check, latest_block_summary, trie_compact, unsparse};
+use subcommands::{archive, check, latest_block_summary, purge, trie_compact, unsparse};
 
 const LOGGING: &str = "logging";
 
@@ -19,6 +19,7 @@ enum DisplayOrder {
     LatestBlock,
     TrieCompact,
     Unsparse,
+    Purge,
 }
 
 fn cli() -> Command<'static> {
@@ -33,6 +34,7 @@ fn cli() -> Command<'static> {
         ))
         .subcommand(trie_compact::command(DisplayOrder::TrieCompact as usize))
         .subcommand(unsparse::command(DisplayOrder::Unsparse as usize))
+        .subcommand(purge::command(DisplayOrder::Purge as usize))
         .arg(
             Arg::new(LOGGING)
                 .short('l')
@@ -71,6 +73,7 @@ fn main() {
         latest_block_summary::COMMAND_NAME => latest_block_summary::run(matches),
         trie_compact::COMMAND_NAME => trie_compact::run(matches),
         unsparse::COMMAND_NAME => unsparse::run(matches),
+        purge::COMMAND_NAME => purge::run(matches),
         _ => unreachable!("{} should be handled above", subcommand_name),
     };
 
--- src/subcommands.rs
+++ src/subcommands.rs
@@ -1,5 +1,6 @@
 pub mod archive;
 pub mod check;
 pub mod latest_block_summary;
+pub mod purge;
 pub mod trie_compact;
 pub mod unsparse;
